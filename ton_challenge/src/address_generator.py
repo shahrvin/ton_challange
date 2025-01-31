04ef07e9e0afbdd8237cbec1ebd21779ec33c9c6
shahrvin <shervinmihan@gmail.com>
2025-02-01 02:44:39 +0330

    generate ton address

---
+++ ton_challenge/src/address_generator.py
@@ -0,0 +1,22 @@
+from tonsdk.contract.wallet import WalletVersionEnum, Wallets
+from tonsdk.utils import bytes_to_b64str
+from tonsdk.crypto import mnemonic_new
+
+
+
+ 
+def ton_wallet_sdk():
+    wallet_workchain = 0
+    wallet_version = WalletVersionEnum.v3r2
+    wallet_mnemonics = mnemonic_new()
+
+    _mnemonics, _pub_k, _priv_k, wallet = Wallets.from_mnemonics(
+        wallet_mnemonics, wallet_version, wallet_workchain)
+    query = wallet.create_init_external_message()
+    base64_boc = bytes_to_b64str(query["message"].to_boc(False))
+
+
+    return  wallet_mnemonics,  wallet.address.to_string() , wallet.address.to_string(True, True, True) , base64_boc
+
+
+
